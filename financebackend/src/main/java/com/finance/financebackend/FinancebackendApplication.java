cc349ee0fda0c0ff711168544d056bd5591aa57f
swastikrout7 <swastikrout1@gmail.com>
2022-04-03 17:43:04 +0530

    fifth commit

--- financebackend/src/main/java/com/finance/financebackend/FinancebackendApplication.java
+++ financebackend/src/main/java/com/finance/financebackend/FinancebackendApplication.java
@@ -3,7 +3,10 @@ package com.finance.financebackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //combination of 3 annotation
+//-enable auto configuration
+//-component scan
+//-configurartion
 public class FinancebackendApplication {
 
 	public static void main(String[] args) {
